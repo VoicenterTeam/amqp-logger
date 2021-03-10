76641fa8bd35219f582a08161ff0d908e72360c1
Maksym Kyrylov <kirillovmax@gmail.com>
2021-03-10 12:52:22 +0200

    singleton config

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 let Logger = require('./Logger');
-
+let _config = null;
 module.exports = {
   fastify: (config) => {
-    let l = Logger(config);
+    if (!_config) {
+      _config = config;
+    }
+    let l = Logger(_config);
     let instance = {};
-    Object.getOwnPropertyNames(config.meth_dict)
+    Object.getOwnPropertyNames(_config.meth_dict)
       .map((item) => {
         instance[item] = (message) => {
-          l.log(config.meth_dict[item], {"Title": item, "Message": message});
+          l.log(_config.meth_dict[item], {"Title": item, "Message": message});
         }
       });
     return instance;
   },
   pastash: (config) => {
-    let l = Logger(config);
+    if (!_config) {
+      _config = config;
+    }
+    let l = Logger(_config);
     let instance = {};
-    Object.getOwnPropertyNames(config.meth_dict)
+    Object.getOwnPropertyNames(_config.meth_dict)
       .map((item) => {
         instance[item] = (message) => {
-          let _m = {"Title": item, "LogLevel": config.meth_dict[item]}
+          let _m = {"Title": item, "LogLevel": _config.meth_dict[item]}
           if (typeof message === "object" && message.hasOwnProperty('Message')) {
             _m = Object.assign(message, _m);
           } else {
             _m.Message = message;
           }
-          l.log(config.meth_dict[item], _m);
+          l.log(_config.meth_dict[item], _m);
         }
       });
     return instance;
